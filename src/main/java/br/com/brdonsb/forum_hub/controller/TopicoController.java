4a9a25a6097b04e7270acff1e1040a05237f25ba
Brandon Marques dos Santos <brdonsb@gmail.com>
2025-07-17 13:49:53 -0300

    funcionaliade deletar topico

--- src/main/java/br/com/brdonsb/forum_hub/controller/TopicoController.java
+++ src/main/java/br/com/brdonsb/forum_hub/controller/TopicoController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -63,5 +64,13 @@ public class TopicoController {
         var topico = repository.getReferenceById(id);
         topico.atualizarInformacoes(dados);
         return ResponseEntity.ok(new DadosDetalhamentoTopico(topico));
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity excluir(@PathVariable Long id){
+        var topico = repository.getReferenceById(id);
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
